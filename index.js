2287362e9531dd1a988971802821f0def6f5d4e9
Azmat Shaikh <azmatshaikh@sdlccorp.com>
2024-03-21 10:08:15 +0530

    changed port

--- index.js
+++ index.js
@@ -55,6 +55,6 @@ app.use(pdfToTxt);
 app.use(wordToPdf)
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(6000, () => {
+  console.log('Server is running on port 6000');
 });
